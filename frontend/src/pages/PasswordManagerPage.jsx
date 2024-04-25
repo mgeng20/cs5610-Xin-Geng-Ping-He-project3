a0d5127eae79d72099dead35ef35f40e83e07ea8
Xin Geng <geng.xin@northeastern.edu>
2024-04-24 23:11:02 -0700

    feat: password manager page rewrite using swr

--- frontend/src/pages/PasswordManagerPage.jsx
+++ frontend/src/pages/PasswordManagerPage.jsx
@@ -9,7 +9,9 @@ import {
   Table,
   message,
 } from "antd";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
 import NavBar from "../components/NavBar";
 import passwordListColumnConfig from "../passwordListColumnConfig";
 import { axiosInstance } from "../util";
@@ -29,142 +31,127 @@ const tailLayout = {
   },
 };
 
-let passwordData;
+const CreateNewPassword = () => {
+  const { trigger, isMutating } = useSWRMutation(
+    ["password-list"],
+    (key, { arg }) => axiosInstance.post("/api/passwords/", arg)
+  );
 
-const PasswordManagerPage = () => {
-  const [form] = Form.useForm();
-  const [passwordsData, setPasswordsData] = useState(null);
-  const [fetchingPasswords, setFetchingPasswords] = useState(false);
-  useEffect(() => {
-    async function fetchInitialPasswords() {
-      try {
-        // Fetch passwords when the component first loads
-        const fetchResponse = await axiosInstance.get("/api/passwords");
-        console.log("Fetched passwords:", fetchResponse.data);
-        setPasswordsData(fetchResponse.data); // Set the passwords data in state
-        setFetchingPasswords(true);
-      } catch (error) {
-        console.error("Failed to fetch passwords:", error);
-      }
-    }
-
-    fetchInitialPasswords();
-  }, []);
-
-  const onFinish = async (values) => {
-    try {
-      const response = await axiosInstance.post("/api/passwords/", {
-        service: values.service,
-        password: values.password,
-      });
-      console.log("Server response:", response.data);
-      message.success("Password saved successfully");
-      const fetchResponse = await axiosInstance.get("/api/passwords");
-      setPasswordsData(fetchResponse.data);
-    } catch (error) {
-      console.error("Failed to create password:", error.response);
-      message.error(
-        "Failed to save the password: " +
-          (error.response.data.message || "Unknown Error")
+  const onFinish = (values) => {
+    trigger(values)
+      .then(() => message.success("Password saved successfully"))
+      .catch((error) =>
+        message.error(
+          "Failed to save the password: " +
+            (error.response.data.message || "Unknown Error")
+        )
       );
-    }
-  };
-
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
   };
 
   return (
+    <Form
+      {...layout}
+      name="basic"
+      style={{
+        maxWidth: 600,
+        margin: "auto",
+        paddingTop: 60,
+      }}
+      initialValues={{
+        alphabet: true,
+        numbers: true,
+        symbols: true,
+      }}
+      onFinish={onFinish}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Service or URL"
+        name="service"
+        rules={[
+          {
+            required: true,
+            message: "Please input the service.",
+          },
+        ]}
+      >
+        <Input placeholder="Enter service name or website url" />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password.",
+          },
+        ]}
+      >
+        <Input.Password
+          placeholder="Enter your password or system will generate one"
+          iconRender={(visible) =>
+            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+          }
+        />
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Space>
+          <Form.Item name="alphabet" valuePropName="checked">
+            <Checkbox>Alphabet</Checkbox>
+          </Form.Item>
+
+          <Form.Item name="numbers" valuePropName="checked">
+            <Checkbox>Numbers</Checkbox>
+          </Form.Item>
+          <Form.Item name="symbols" valuePropName="checked">
+            <Checkbox>Symbols</Checkbox>
+          </Form.Item>
+          <Form.Item label="Length" name="length">
+            <InputNumber
+              min={4}
+              max={50}
+              defaultValue={12}
+              style={{
+                width: 56,
+              }}
+            />
+          </Form.Item>
+        </Space>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Space>
+          <Button type="primary" htmlType="submit" isLoading={isMutating}>
+            Submit
+          </Button>
+          <Button htmlType="reset">Clear</Button>
+        </Space>
+      </Form.Item>
+    </Form>
+  );
+};
+
+const PasswordTable = () => {
+  const { data, isLoading } = useSWR(["password-list"], () =>
+    axiosInstance.get("/api/passwords").then((res) => res.data)
+  );
+  return (
+    <Table
+      isLoading={isLoading}
+      style={{ margin: 20, marginTop: 60 }}
+      columns={passwordListColumnConfig}
+      dataSource={data}
+    />
+  );
+};
+
+const PasswordManagerPage = () => {
+  return (
     <>
       <NavBar />
-      <Form
-        {...layout}
-        name="basic"
-        style={{
-          maxWidth: 600,
-          margin: "auto",
-          paddingTop: 60,
-        }}
-        initialValues={{
-          alphabet: true,
-          numbers: true,
-          symbols: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Service or URL"
-          name="service"
-          rules={[
-            {
-              required: true,
-              message: "Please input the service.",
-            },
-          ]}
-        >
-          <Input placeholder="Enter service name or website url" />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password.",
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder="Enter your password or system will generate one"
-            iconRender={(visible) =>
-              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-            }
-          />
-        </Form.Item>
-        <Form.Item {...tailLayout}>
-          <Space>
-            <Form.Item name="alphabet" valuePropName="checked">
-              <Checkbox>Alphabet</Checkbox>
-            </Form.Item>
-
-            <Form.Item name="numbers" valuePropName="checked">
-              <Checkbox>Numbers</Checkbox>
-            </Form.Item>
-            <Form.Item name="symbols" valuePropName="checked">
-              <Checkbox>Symbols</Checkbox>
-            </Form.Item>
-            <Form.Item label="Length" name="length">
-              <InputNumber
-                min={4}
-                max={50}
-                defaultValue={12}
-                style={{
-                  width: 56,
-                }}
-              />
-            </Form.Item>
-          </Space>
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Space>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-            <Button htmlType="reset">Clear</Button>
-          </Space>
-        </Form.Item>
-      </Form>
-      <div>
-        <Table
-          style={{ margin: 20, marginTop: 60 }}
-          columns={passwordListColumnConfig}
-          dataSource={passwordsData}
-        />
-      </div>
+      <CreateNewPassword />
+      <PasswordTable />
     </>
   );
 };
