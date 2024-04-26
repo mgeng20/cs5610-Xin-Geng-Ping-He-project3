bacfaa51be131cf02d29fad7d881026a784553df
Xin Geng <geng.xin@northeastern.edu>
2024-04-26 00:22:14 -0700

    add drawer

--- frontend/src/components/UsernameMenuItem.jsx
+++ frontend/src/components/UsernameMenuItem.jsx
@@ -1,9 +1,17 @@
-import { Menu } from "antd";
+import { Drawer, Menu } from "antd";
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../util";
 
 export default () => {
   const [user, setUser] = useState();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const showDrawer = () => {
+    setDrawerOpen(true);
+  };
+  const onClose = () => {
+    setDrawerOpen(false);
+  };
 
   useEffect(() => {
     axiosInstance
@@ -15,6 +23,13 @@ export default () => {
         console.log(e);
       });
   }, []);
-
-  return <Menu.Item>Hello, {user?.username} </Menu.Item>;
+  // get message list
+  return (
+    <>
+      <Menu.Item onClick={showDrawer}>Hello, {user?.username} </Menu.Item>
+      <Drawer title="Messages" onClose={onClose} open={drawerOpen}>
+        <p>Empty...</p>
+      </Drawer>
+    </>
+  );
 };
