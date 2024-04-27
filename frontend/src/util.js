2046b5e6808f0b23fd23eda4ebfccdaca9f9d6da
Xin Geng <geng.xin@northeastern.edu>
2024-04-26 20:11:48 -0700

    feat: prod url

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -2,8 +2,13 @@ import axios from "axios";
 import _ from "lodash";
 import { mutate } from "swr";
 
+const LOCAL_URL = "http://localhost:8000";
+const PROD_URL = "http://ec2-35-90-16-29.us-west-2.compute.amazonaws.com:8000";
+
+console.log("NODE_ENV", process.env.NODE_ENV);
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.NODE_ENV === "production" ? PROD_URL : LOCAL_URL,
 });
 
 axiosInstance.interceptors.request.use(
